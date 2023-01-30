511454a9d3003c0070db1b6167faf0f9f807e951
Nathan Dejesus <100551611+AvalonAce@users.noreply.github.com>
2023-01-29 19:00:39 -0500

    Added Front-End code base.

---
+++ go-goal-front/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarTopComponent } from './navbar/navbar-top/navbar-top.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarTopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
