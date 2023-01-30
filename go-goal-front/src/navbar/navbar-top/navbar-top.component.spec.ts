f7ff12d2fc6ae96e45aa7fce1cafe060a1406455
Nathan Dejesus <100551611+AvalonAce@users.noreply.github.com>
2023-01-30 16:26:42 -0500

    Add files via upload

---
+++ go-goal-front/src/navbar/navbar-top/navbar-top.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavbarTopComponent } from './navbar-top.component';
+
+
+describe('NavbarTopComponent', () => {
+  let component: NavbarTopComponent;
+  let fixture: ComponentFixture<NavbarTopComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NavbarTopComponent],
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(NavbarTopComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
