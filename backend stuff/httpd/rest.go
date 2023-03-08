29af9f26a8c8d707a827af4f46ee49a0de1d1b7d
Oscar Rodas <60824325+thescar101@users.noreply.github.com>
2023-03-07 19:45:25 -0500

    Changed checklogin from a get to a post
    Seems to be best practice in web development

--- backend stuff/httpd/rest.go
+++ backend stuff/httpd/rest.go
@@ -26,7 +26,7 @@ func httpHandler() http.Handler {
 	s.HandleFunc("/users/{id}/lastname", handler.UpdateLastname(globalDB)).Methods("PUT")
 	s.HandleFunc("/users/{id}/email", handler.UpdateEmail(globalDB)).Methods("PUT")
 	s.HandleFunc("/users/{id}/password", handler.UpdatePassword(globalDB)).Methods("PUT")
-	s.HandleFunc("/login", handler.CheckLogin(globalDB)).Methods("GET")
+	s.HandleFunc("/login", handler.CheckLogin(globalDB)).Methods("POST")
 
 	// Create and Retrieve Goals
 	s.HandleFunc("/goals", handler.CreateGoal(globalDB)).Methods("POST")
