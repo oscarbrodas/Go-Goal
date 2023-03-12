5d5b24821502b1f4a28cef9a4943437fd4326875
Nathan Dejesus <100551611+AvalonAce@users.noreply.github.com>
2023-03-12 12:25:09 -0400

    User Page Routing Structure Added

--- go-goal-front/src/app/app-routing.module.ts
+++ go-goal-front/src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { MainComponent } from './main/main.component';
 import { LoginPageComponent } from './login-page/login-page.component'
 import { SignUpComponentComponent } from './sign-up-component/sign-up-component.component'
 import { NotFoundComponent } from './not-found/not-found.component';
+import { UserModule } from './user/user.module';
+import { UserComponent } from './user/user/user.component';
+import { GoalsComponent } from './user/goals/goals.component';
 
 const routes: Routes = [
   { path: 'main', component: MainComponent },
@@ -12,12 +15,18 @@ const routes: Routes = [
   { path: 'sign-up', component: SignUpComponentComponent },
   { path: 'aboutus', component: NotFoundComponent },
   { path: 'help', component: NotFoundComponent },
+  {
+    path: 'user/:id', component: UserComponent, children: [
+      { path: 'goals', component: GoalsComponent },
+
+    ]
+  },
   //Must leave 404 redirect as last route on page
   { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), UserModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- go-goal-front/src/app/user/user.module.ts
+++ go-goal-front/src/app/user/user.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GoalsComponent } from './goals/goals.component';
+import { UserComponent } from './user/user.component';
 
 // CREATE ALL USER LOGGED IN COMPONENTS HERE
 
 @NgModule({
   declarations: [
-    GoalsComponent
+    GoalsComponent,
+    UserComponent
   ],
   imports: [
     CommonModule
