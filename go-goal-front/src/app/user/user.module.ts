83c228fb0d31cb27dc5c0cbd0ab15ef68e30da0b
Nathan Dejesus <100551611+AvalonAce@users.noreply.github.com>
2023-03-12 21:03:00 -0400

    User Module fix (added routing)

--- go-goal-front/src/app/user/user.module.ts
+++ go-goal-front/src/app/user/user.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { UserModuleRoutingModule } from './user-module-routing.module';
+import { UserRoutingModule } from './user-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    UserModuleRoutingModule
+    UserRoutingModule
   ]
 })
-export class UserModuleModule { }
+export class UserModule { }
