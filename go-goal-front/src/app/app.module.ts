8fe6600226a74db62e5726f1b2c349f2b37cfdca
Nathan Dejesus <100551611+AvalonAce@users.noreply.github.com>
2023-03-16 11:54:05 -0400

    Mat Icon Add

--- go-goal-front/src/app/app.module.ts
+++ go-goal-front/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { CarouselModule, CardComponent, CardModule } from '@coreui/angular';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -40,6 +41,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     MatToolbarModule,
     MatDialogModule,
     MatButtonModule,
+    MatIconModule,
     HttpClientModule,
     FormsModule,
     CarouselModule,
