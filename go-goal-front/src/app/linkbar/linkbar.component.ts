dd2e2204ee7850ef62aa5c11426f8c39890f1cfa
Nathan Dejesus <100551611+AvalonAce@users.noreply.github.com>
2023-04-02 11:09:36 -0400

    CSS Update Several Places

--- go-goal-front/src/app/linkbar/linkbar.component.ts
+++ go-goal-front/src/app/linkbar/linkbar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges, Input } from '@angular/core';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'app-linkbar',
   templateUrl: './linkbar.component.html',
   styleUrls: ['./linkbar.component.css']
 })
-export class LinkbarComponent {
+export class LinkbarComponent implements OnInit, OnChanges {
+
+  @Input() loggedIn: boolean = false;
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.loggedIn = this.userService.isLoggedIn();
+
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+  }
 
 }
