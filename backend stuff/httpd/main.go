90f48f44325f84311f6f3b08d7664a095ac1cc3d
Oscar Rodas <60824325+thescar101@users.noreply.github.com>
2023-04-17 21:54:34 -0400

    Avatar processing backend (#50)
    * AWS S3 File System for avatar files
    
    Two new routes have been added.
    
    /users/{id}/avatar
    put: Updates the user's avatar, takes in a string which needs to be the image's base64 representation
    
    get: returns the base64 representation of the avatar to be displayed on the webpage.
    
    To use this, a .env file with the AWS keys must be added to the httpd directory. Sent in the discord.
    
    * Updated Create User
    
    CreateUser now initializes the avatar to the starter one. Changes made to accommodate the change.

--- backend stuff/httpd/main.go
+++ backend stuff/httpd/main.go
@@ -8,16 +8,33 @@ import (
 
 	"go-goal/httpd/handler"
 
+	"github.com/joho/godotenv"
+
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 var globalDB *gorm.DB
+var globalUploader *manager.Uploader
+var globalDownloader *manager.Downloader
 
 // mySQL DSN string is no longer needed since we are using sqlite
 // const DSN string = "root:password@tcp(127.0.0.1:3306)/somedb?charset=utf8mb4&parseTime=True&loc=Local"
 
 func main() {
+	// Load Environment Variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Setup SQlite DB
 	db, err := gorm.Open(sqlite.Open("main.db"), &gorm.Config{})
 	if err != nil {
 		panic("database failed to open")
@@ -30,8 +47,21 @@ func main() {
 	globalDB.AutoMigrate(&handler.Friend{})
 	globalDB.AutoMigrate(&handler.Benchmark{})
 
+	// Setup s3 uploader
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Printf("error: %v", err)
+		return
+	}
+
+	client := s3.NewFromConfig(cfg)
+	globalUploader = manager.NewUploader(client)
+	globalDownloader = manager.NewDownloader(client)
+
+	// Start Router
 	host := "127.0.0.1:9000" // :9000 is the port
 	if err := http.ListenAndServe(host, httpHandler()); err != nil {
 		log.Fatalf("Failed to listen on %s: %v", host, err)
 	}
+
 }
