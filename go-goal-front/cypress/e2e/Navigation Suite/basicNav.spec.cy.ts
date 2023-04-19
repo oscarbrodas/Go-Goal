1dd8113b06ede51ce79a515445cb901ebb808476
ThatOneGuySam <115837623+ThatOneGuySam@users.noreply.github.com>
2023-04-19 10:20:53 -0400

    FAQ and About Us Tests
    Added cypress test to ensure navigation to and from faq and about us pages work properly.

--- go-goal-front/cypress/e2e/Navigation Suite/basicNav.spec.cy.ts
+++ go-goal-front/cypress/e2e/Navigation Suite/basicNav.spec.cy.ts
@@ -11,7 +11,7 @@ describe('Navigation from Main Page Tests', () => {
     cy.url().should('include','sign-up')
   })
   it('Visit Home from bottom bar', () => {
-    cy.get('[name = "linkBarMain"]').click()
+    cy.get('[name = "linkBarHome"]').click()
     cy.url().should('include','main')
   })
 
@@ -53,7 +53,7 @@ describe('Navigation from Login Page Tests', () => {
     cy.url().should('include','sign-up')
   })
   it('Visit Home from bottom bar', () => {
-    cy.get('[name = "linkBarMain"]').click()
+    cy.get('[name = "linkBarHome"]').click()
     cy.url().should('include','main')
   })
 
@@ -95,7 +95,91 @@ describe('Navigation from Sign Up Page Tests', () => {
     cy.url().should('include','sign-up')
   })
   it('Visit Home from bottom bar', () => {
-    cy.get('[name = "linkBarMain"]').click()
+    cy.get('[name = "linkBarHome"]').click()
+    cy.url().should('include','main')
+  })
+
+  it('Visit Help from bottom bar', ()=>{
+    cy.get('[name = "linkBarHelp"]').click()
+    cy.url().should('include','help')
+  })
+
+  it('Visit About Us from bottom bar', ()=>{
+    cy.get('[name = "linkBarAboutUs"]').click()
+    cy.url().should('include','aboutus')
+  })
+
+  it('Visit login page from bottom bar', ()=>{
+    cy.get('[name = "linkBarLogin"]').click()
+    cy.url().should('include','login')
+  })
+
+  it('Visit sign-up page from bottom bar', ()=>{
+    cy.get('[name = "linkBarSignUp"]').click()
+    cy.url().should('include','sign-up')
+  })
+})
+
+describe('Navigation from FAQ Page Tests', () => {
+  beforeEach(() => {
+    cy.visit('/help')
+  })
+  it('Visits the initial project page', () => {
+    cy.url().should('include','help')
+  })
+
+  it('Visit login page from top bar', ()=>{
+    cy.get('[id = "navBarLogin"]').click()
+    cy.url().should('include','login')
+  })
+  it('Visit sign-up page from top bar', ()=>{
+    cy.get('[id = "navBarSignUp"]').click()
+    cy.url().should('include','sign-up')
+  })
+  it('Visit Home from bottom bar', () => {
+    cy.get('[name = "linkBarHome"]').click()
+    cy.url().should('include','main')
+  })
+
+  it('Visit Help from bottom bar', ()=>{
+    cy.get('[name = "linkBarHelp"]').click()
+    cy.url().should('include','help')
+  })
+
+  it('Visit About Us from bottom bar', ()=>{
+    cy.get('[name = "linkBarAboutUs"]').click()
+    cy.url().should('include','aboutus')
+  })
+
+  it('Visit login page from bottom bar', ()=>{
+    cy.get('[name = "linkBarLogin"]').click()
+    cy.url().should('include','login')
+  })
+
+  it('Visit sign-up page from bottom bar', ()=>{
+    cy.get('[name = "linkBarSignUp"]').click()
+    cy.url().should('include','sign-up')
+  })
+})
+
+describe('Navigation from About Us Page Tests', () => {
+  beforeEach(() => {
+    cy.visit('/aboutus')
+  })
+  it('Visits the initial project page', () => {
+    cy.url().should('include','aboutus')
+  })
+
+  it('Visit login page from top bar', ()=>{
+    cy.get('[id = "navBarLogin"]').click()
+    cy.url().should('include','login')
+  })
+  it('Visit sign-up page from top bar', ()=>{
+    cy.get('[id = "navBarSignUp"]').click()
+    cy.url().should('include','sign-up')
+  })
+  it('Visit Home from bottom bar', () => {
+    cy.get('[name = "linkBarHome"]').click()
     cy.url().should('include','main')
   })
 
